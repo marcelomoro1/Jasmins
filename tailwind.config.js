9a29522c60151e2cf4b7b97dadc92085248327ea
Marcelo Moro <101741773+marcelomoro1@users.noreply.github.com>
2025-02-25 17:33:23 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: [
+      './index.html', // Para processar o arquivo HTML
+      './src/**/*.{js,jsx,ts,tsx}', // Para processar arquivos JS/JSX dentro da pasta src
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
